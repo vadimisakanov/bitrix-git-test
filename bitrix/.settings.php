cdd33f83c9dd8c6daab6d5ad0a6a80936bfd62e7
Vadim Isakanov <v.isakanov@southbridge.ru>
2018-06-27 19:04:52 +0300

    added bitrix default files

---
+++ bitrix/.settings.php
@@ -0,0 +1,86 @@
+<?php
+return array (
+'pull_s1' => 'BEGIN GENERATED PUSH SETTINGS. DON\'T DELETE COMMENT!!!!',
+'pull' => Array(
+    'value' =>  array(
+        'path_to_listener' => 'http://#DOMAIN#/bitrix/sub/',
+        'path_to_listener_secure' => 'https://#DOMAIN#/bitrix/sub/',
+        'path_to_modern_listener' => 'http://#DOMAIN#/bitrix/sub/',
+        'path_to_modern_listener_secure' => 'https://#DOMAIN#/bitrix/sub/',
+        'path_to_mobile_listener' => 'http://#DOMAIN#:8893/bitrix/sub/',
+        'path_to_mobile_listener_secure' => 'https://#DOMAIN#:8894/bitrix/sub/',
+        'path_to_websocket' => 'ws://#DOMAIN#/bitrix/subws/',
+        'path_to_websocket_secure' => 'wss://#DOMAIN#/bitrix/subws/',
+        'path_to_publish' => 'http://vs600.xpage.ru:8895/bitrix/pub/',
+        'nginx_version' => '3',
+        'nginx_command_per_hit' => '100',
+        'nginx' => 'Y',
+        'nginx_headers' => 'N',
+        'push' => 'Y',
+        'websocket' => 'Y',
+        'signature_key' => '9F7a16PAp7sKJFnI5TpiPoOQrFFtgxALI1P5Ijl2uF5tmYDynJCFSlU5aYkusGBHKfclfyOsaEP3mTNZIrOcIUOUZLRvzKDwjg8pttLxT8vjnr3RbEKCuj89adbJj7pK',
+        'signature_algo' => 'sha1',
+        'guest' => 'N',
+    ),
+),
+'pull_e1' => 'END GENERATED PUSH SETTINGS. DON\'T DELETE COMMENT!!!!',
+  'utf_mode' =>
+  array (
+    'value' => true,
+    'readonly' => true,
+  ),
+  'cache_flags' =>
+  array (
+    'value' =>
+    array (
+      'config_options' => 3600,
+      'site_domain' => 3600,
+    ),
+    'readonly' => false,
+  ),
+  'cookies' =>
+  array (
+    'value' =>
+    array (
+      'secure' => false,
+      'http_only' => true,
+    ),
+    'readonly' => false,
+  ),
+  'exception_handling' =>
+  array (
+    'value' =>
+    array (
+      'debug' => false,
+      'handled_errors_types' => 4437,
+      'exception_errors_types' => 4437,
+      'ignore_silence' => false,
+      'assertion_throws_exception' => true,
+      'assertion_error_type' => 256,
+      'log' => array (
+          'settings' =>
+          array (
+            'file' => '/var/log/php/exceptions.log',
+            'log_size' => 1000000,
+        ),
+      ),
+    ),
+    'readonly' => false,
+  ),
+  'connections' =>
+  array (
+    'value' =>
+    array (
+      'default' =>
+      array (
+        'className' => '\\Bitrix\\Main\\DB\\MysqliConnection',
+        'host' => 'localhost',
+        'database' => 'sitemanager',
+        'login'    => 'bitrix0',
+        'password' => 'lda0MLrdA6Bouekoxg5O',
+        'options' => 2,
+      ),
+    ),
+    'readonly' => true,
+  )
+);
